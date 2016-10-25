4d9746cb94c0a068adeac25ef949abd814851354
liubitao <767205795@qq.com>
2016-10-25 17:39:05 +0800

    分享cell完成

---
+++ knock Honey/晒单/晒单首页/view/KHAppearTableViewCell.h
@@ -0,0 +1,26 @@
+//
+//  KHAppearTableViewCell.h
+//  knock Honey
+//
+//  Created by 刘毕涛 on 16/10/25.
+//  Copyright © 2016年 liubitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHAppearTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *winPic;
+@property (weak, nonatomic) IBOutlet UILabel *winName;
+@property (weak, nonatomic) IBOutlet UILabel *winTitle;
+@property (weak, nonatomic) IBOutlet UILabel *proudceName;
+@property (weak, nonatomic) IBOutlet UILabel *qishu;
+@property (weak, nonatomic) IBOutlet UILabel *winContent;
+@property (weak, nonatomic) IBOutlet UIImageView *sharePic;
+@property (weak, nonatomic) IBOutlet UIImageView *pic1;
+@property (weak, nonatomic) IBOutlet UIImageView *pic2;
+@property (weak, nonatomic) IBOutlet UIImageView *pic3;
+@property (weak, nonatomic) IBOutlet UILabel *time;
+@property (weak, nonatomic) IBOutlet UILabel *number;
+@property (weak, nonatomic) IBOutlet UIImageView *zanPic;
+
+@end
---
+++ knock Honey/晒单/晒单首页/view/KHAppearTableViewCell.m
@@ -0,0 +1,21 @@
+//
+//  KHAppearTableViewCell.m
+//  knock Honey
+//
+//  Created by 刘毕涛 on 16/10/25.
+//  Copyright © 2016年 liubitao. All rights reserved.
+//
+
+#import "KHAppearTableViewCell.h"
+
+@implementation KHAppearTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
